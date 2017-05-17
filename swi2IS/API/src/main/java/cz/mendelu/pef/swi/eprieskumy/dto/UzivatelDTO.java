8f320a76cad4e0d35358b0f752abefeb5a48bdb1
VeroAl <almasiova.veronika@gmail.com>
2017-05-17 22:44:20 +0200

    Add files via upload

---
+++ swi2IS/API/src/main/java/cz/mendelu/pef/swi/eprieskumy/dto/UzivatelDTO.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.mendelu.pef.swi.eprieskumy.dto;
+
+/**
+ *
+ * @author veronika.almasiova
+ */
+public class UzivatelDTO {
+    
+    private int id;
+    
+    private String nameUzivatel; 
+    
+    private String name;
+    
+    private String surname;
+
+    private String ICO;
+ 
+    private String titul;
+
+    private String place;
+
+    private String email;
+
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameUzivatel() {
+        return nameUzivatel;
+    }
+
+    public void setNameUzivatel(String nameUzivatel) {
+        this.nameUzivatel = nameUzivatel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getICO() {
+        return ICO;
+    }
+
+    public void setICO(String ICO) {
+        this.ICO = ICO;
+    }
+
+    public String getTitul() {
+        return titul;
+    }
+
+    public void setTitul(String titul) {
+        this.titul = titul;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+}
+
