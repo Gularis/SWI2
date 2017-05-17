64fa7a3eb4ab1cd6e573ee206312c180b94bd7bd
VeroAl <almasiova.veronika@gmail.com>
2017-05-17 23:24:08 +0200

    Add files via upload

---
+++ swi2IS/WebModuleFrontend/src/main/java/cz/mendelu/pef/swi/webmodulefrontend/mvc/controller/HomepageController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.mendelu.pef.swi.webmodulefrontend.mvc.controller;
+
+import java.security.Principal;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author veronika.almasiova
+ */
+@Controller
+@RequestMapping("/")
+public class HomepageController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model, Principal principal) {
+      
+        return "home";
+    }
+    
+    
+}
+
